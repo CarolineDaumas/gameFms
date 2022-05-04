510add21ece2ff24e0237dc0beb01f527ab0e774
CarolineDaumas <caroline.daumas@fms-ea.com>
2022-05-04 09:40:56 +0200

    ajout tableau

---
+++ script.js
@@ -0,0 +1,6 @@
+let tab=["oiseau","avocat", "kiwi", "fraise", "poire","chocolat","orange","Italie", "koala", "bateau", 
+"Groenland", "stylo","pain", "grenadine","chien", "imprimante", "chat", "conceptuellement", "chocolatine","pain au chocolat"]
+
+tab.forEach(element => {
+    console.log(element)
+    });
